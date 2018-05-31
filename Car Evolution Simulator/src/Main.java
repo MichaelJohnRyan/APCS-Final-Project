f926ee5ce5c1315852ea84eb256cb844841bda06
ryanm <ryanm@SHS-206-05.spartatwp-k12.local>
2018-05-31 14:05:40 -0400

    5/31/18

--- Car Evolution Simulator/src/Main.java
+++ Car Evolution Simulator/src/Main.java
@@ -1,10 +1,17 @@
+import org.dyn4j.*;
+import java.awt.*;
+import javax.swing.*;
 
-public class Main {
-
-	public static void main(String[] args) {
-		System.out.println("Test");
-
+public class Main extends JFrame {
+	public Main() {
+		super("Main");
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setSize(2400, 1080);
 	}
-
+	
+	public static void main(String[] args) {
+		new Main();
+	}
 }
-// https://www.youtube.com/watch?v=ptK9-CNms98
+//https://www.youtube.com/watch?v=ptK9-CNms98
