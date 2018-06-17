420b19daca78cc1e16de719c4110beeb01388fcb
mjrjr <mjrjr@10.0.1.20>
2018-06-17 17:55:20 -0400

    a

--- Car Evolution Simulator/src/Main.java
+++ Car Evolution Simulator/src/Main.java
@@ -25,47 +25,58 @@ public class Main extends SimulationFrame {
 		simulation.run();
 	}
 	
-	public void createCar(){
+	public Car createCar(){
+		//car variable randomizers
+		double bodyWidth = (Math.random()*10)+10;
+		double bodyHeight = (Math.random()*5)+5;
+		double wheel1Radius = (Math.random()*4)+1;
+		double wheel2Radius = (Math.random()*4)+1;
+		double motor1Torque = (Math.random()*500)+500;
+		double motor2Torque = (Math.random()*500)+500;
+		double motor1Speed = (Math.random()*40)+10;
+		double motor2Speed = (Math.random()*40)+10;
+		//creates the car object...
+		Car car = new Car(bodyWidth, bodyHeight, wheel1Radius, wheel2Radius, motor1Torque, motor2Torque, motor1Speed, motor2Speed);
+		//...then returns it
+		return car;
 		
-		double width = (Math.random()*10)+10;
-		double height = (Math.random()*5)+5;
-		double wheel1Radius = (Math.random()*6) +1;
-		double wheel2Radius = (Math.random()*6) +1;
-		
+	}
+	public void createBody(Car car){
+		//makes each of the components of the car based on the car object and adds them to the simulation
 		SimulationBody wheel1 = new SimulationBody();
-		wheel1.addFixture(Geometry.createCircle(wheel1Radius), 1, 20, .125);
+		wheel1.addFixture(Geometry.createCircle(car.getWheel1Radius()), 1, 20, .125);
 		wheel1.setMass(MassType.NORMAL);
 		this.world.addBody(wheel1);
 		
 		SimulationBody wheel2 = new SimulationBody();
-		wheel2.addFixture(Geometry.createCircle(wheel2Radius), 1, 20, .125);
+		wheel2.addFixture(Geometry.createCircle(car.getWheel2Radius()), 1, 20, .125);
 		wheel2.setMass(MassType.NORMAL);
-		wheel2.translate(new Vector2(width, 0));
+		wheel2.translate(new Vector2(car.getBodyWidth(), 0));
 		this.world.addBody(wheel2);
 		
 		SimulationBody body = new SimulationBody();
-		body.addFixture(Geometry.createPolygon(new Vector2(0, 0), new Vector2(width, 0), new Vector2(width, height), new Vector2(0,height)));
+		body.addFixture(Geometry.createPolygon(new Vector2(0, 0), new Vector2(car.getBodyWidth(), 0), new Vector2(car.getBodyWidth(), car.getBodyHeight()), new Vector2(0,car.getBodyHeight())));
 		body.setMass(MassType.NORMAL);
 		this.world.addBody(body);
 		
-		WheelJoint joint = new WheelJoint(body, wheel1, new Vector2(0,0), new Vector2(wheel1Radius,wheel1Radius));
+		WheelJoint joint = new WheelJoint(body, wheel1, new Vector2(0,0), new Vector2(car.getWheel1Radius(),car.getWheel1Radius()));
 		joint.setMotorEnabled(true);
-		joint.setMotorSpeed(10);
-		joint.setMaximumMotorTorque(500);
+		joint.setMotorSpeed(100);
+		joint.setMaximumMotorTorque(10);
 		joint.setCollisionAllowed(false);
 		this.world.addJoint(joint);
 		
-		WheelJoint joint2 = new WheelJoint(body, wheel2, new Vector2(width, 0), new Vector2(wheel2Radius, wheel2Radius));
+		WheelJoint joint2 = new WheelJoint(body, wheel2, new Vector2(car.getBodyWidth(), 0), new Vector2(car.getWheel2Radius(), car.getWheel2Radius()));
 		joint2.setMotorEnabled(true);
 		joint2.setMotorSpeed(10);
 		joint2.setMaximumMotorTorque(500);
 		joint2.setCollisionAllowed(false);
 		this.world.addJoint(joint2);
-		
 	}
 
 	@Override
 	protected void initializeWorld() {
+		//IGNORE... (its a square used for testing)
 		/*
 		SimulationBody body1 = new SimulationBody();
 		body1.addFixture(Geometry.createSquare(10), 1, 20, .25);
@@ -74,16 +85,16 @@ public class Main extends SimulationFrame {
 		body1.setMass(MassType.NORMAL);
 		this.world.addBody(body1);
 		*/
-		
+		//adds the floor
 		SimulationBody floor = new SimulationBody();
 		floor.addFixture(Geometry.createRectangle(1000.0, 0.5));
 		floor.setMass(MassType.INFINITE);
 		floor.translate(0, -50);
 		this.world.addBody(floor);
 
-		createCar();
-		
-		this.world.setGravity(World.EARTH_GRAVITY);
+		createBody(createCar());
+		//sets the worlds gravity to something that I think is closer to earth's gravity
+		this.world.setGravity(new Vector2(0, -30));
 	}
 }
 //www.youtube.com/watch?v=ptK9-CNms98
