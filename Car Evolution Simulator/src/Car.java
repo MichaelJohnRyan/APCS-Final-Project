4f134a12578e21f2c1b17aa329281a45ec75dde5
mjrjr <mjrjr@10.0.1.20>
2018-06-17 17:32:59 -0400

    a

--- Car Evolution Simulator/src/Car.java
+++ Car Evolution Simulator/src/Car.java
@@ -14,7 +14,7 @@ import org.dyn4j.samples.framework.SimulationFrame;
 public class Car {
 	
 	private double bodyWidth;
-	private double bodyheight;
+	private double bodyHeight;
 	private double wheel1Radius;
 	private double wheel2Radius;
 	private double motor1Torque;
@@ -24,7 +24,7 @@ public class Car {
 	
 	public Car(double bodyWidth, double bodyHeight, double wheel1Radius, double wheel2Radius, double motor1Torque, double motor2Torque, double motor1Speed, double motor2Speed){
 		this.bodyWidth = bodyWidth;
-		this.bodyheight = bodyHeight;
+		this.bodyHeight = bodyHeight;
 		this.wheel1Radius = wheel1Radius;
 		this.wheel2Radius = wheel2Radius;
 		this.motor1Torque = motor1Torque;
@@ -32,4 +32,30 @@ public class Car {
 		this.motor1Speed = motor1Speed;
 		this.motor2Speed = motor2Speed;
 	}
+	//getters
+	public double getBodyWidth(){
+		return bodyWidth;
+	}
+	public double getBodyHeight(){
+		return bodyHeight;
+	}
+	public double getWheel1Radius(){
+		return wheel1Radius;
+	}
+	public double getWheel2Radius(){
+		return wheel2Radius;
+	}
+	public double getMotor1Torque(){
+		return motor1Torque;
+	}
+	public double getMotor2Torque(){
+		return motor2Torque;
+	}
+	public double getMotor1Speed(){
+		return motor1Speed;
+	}
+	public double getMotor2Speed(){
+		return motor2Speed;
+	}
+	//setters
 }
